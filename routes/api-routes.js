5ea7601f16ba266a6e9a95786b706559cab06fcd
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-05-26 22:00:54 -0400

    updated model and routes

---
+++ routes/api-routes.js
@@ -0,0 +1,106 @@
+// const router = require('express').Router();
+const Workout = require('../models/workout.js')
+
+module.exports = (app) => {
+    app.get('/api/workouts', (req, res) => {
+        Workout.find({})
+        .then(dbWorkout => {
+            //console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    });
+    
+    app.post('/api/workouts', ({ body }, res) => {
+        Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    });
+    
+    app.put('/api/workouts/:id', (req, res) => {
+        Workout.update(
+            {
+                _id: req.params.id
+            },
+            {
+                $push: {
+                    workouts: req.body
+                }
+            }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    });
+    
+    app.get('/api/workouts/range', (req, res) => {
+        Workout.find({})
+        .sort({__id: -1})
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch (err => {
+            res.status(400).json(err)
+        });
+    });
+}
+
+
+
+//From 07
+// app.post("/update/:id", (req, res) => {
+//     db.notes.update(
+//       {
+//         _id: mongojs.ObjectId(req.params.id)
+//       },
+//       {
+//         $set: {
+//           title: req.body.title,
+//           note: req.body.note,
+//           modified: Date.now()
+//         }
+//       },
+//       (error, data) => {
+//         if (error) {
+//           res.send(error);
+//         } else {
+//           res.send(data);
+//         }
+//       }
+//     );
+// });
+
+//From 09
+
+// app.put("/markread/:id", ({ params }, res) => {
+//     db.books.update(
+//       {
+//         _id: mongojs.ObjectId(params.id)
+//       },
+//       {
+//         $set: {
+//           read: true
+//         }
+//       },
+  
+//       (error, edited) => {
+//         if (error) {
+//           console.log(error);
+//           res.send(error);
+//         } else {
+//           console.log(edited);
+//           res.send(edited);
+//         }
+//       }
+//     );
+// });
--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
 require("./routes/html-routes")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
