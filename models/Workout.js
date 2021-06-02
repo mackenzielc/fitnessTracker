76c2664ae4b33611adc3593eaf0c04acc91cd41d
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-06-01 23:42:47 -0400

    deploying

--- models/Workout.js
+++ models/Workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const WorkoutSchema = new Schema({
     day: {
         type: Date, 
         default: Date.now
@@ -37,6 +37,6 @@ const workoutSchema = new Schema({
     ]
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout
