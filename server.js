9fecb3b6003f3028497a5eb263c11c434bb18875
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-06-02 19:04:23 -0400

    updated server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+// routes
+const htmlRoutes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,10 +19,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
   useFindAndModify: false
 });
-
-// routes
-const htmlRoutes = require("./routes/html-routes");
-const apiRoutes = require("./routes/api-routes");
 
 // route handlers
 app.use('/', htmlRoutes);
